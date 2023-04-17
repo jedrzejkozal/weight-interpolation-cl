a0437a738021ff7c7c348aad6bbca083fbd85f5c
jedrzejkozal <jedrzej.kozal@gmail.com>
2023-04-17 17:24:01 +0200

    do not log models

--- utils/training.py
+++ utils/training.py
@@ -146,9 +146,9 @@ def train(model: ContinualModel, dataset: ContinualDataset,
         if hasattr(model, 'end_task'):
             model.end_task(dataset)
 
-        if 'clewi' in model.NAME and os.path.exists('old_model.pt') and os.path.exists('net.pt'):
-            logger.log_artifact('old_model.pt', f'old_model_task_{t}')
-            logger.log_artifact('net.pt', f'net_model_task_{t}')
+        # if 'clewi' in model.NAME and os.path.exists('old_model.pt') and os.path.exists('net.pt'):
+        #     logger.log_artifact('old_model.pt', f'old_model_task_{t}')
+        #     logger.log_artifact('net.pt', f'net_model_task_{t}')
 
         accs = evaluate(model, dataset)
         results.append(accs[0])
