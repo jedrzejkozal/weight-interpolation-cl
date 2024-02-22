ac7ecc1f2ed8fd800df5f5500ac80927d3acd223
jedrzejkozal <jedrzej.kozal@gmail.com>
2024-02-22 11:47:14 +0100

    change run_template.sh

--- run_template.sh
+++ run_template.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if [[ $(conda config --show | grep "solver: classic") == "solver: classic" ]]; then
-    source activate base
-    conda install -n base conda-libmamba-solver -y
-    conda config --set solver libmamba
-fi
 
 if { conda env list | grep 'interpolation'; } >/dev/null 2>&1; then
     echo "env exists"
 else
+    if [[ $(conda config --show | grep "solver: classic") == "solver: classic" ]]; then
+        source activate base
+        conda install -n base conda-libmamba-solver -y
+        conda config --set solver libmamba
+    fi
     conda env create -f env.yml -y
 fi
 source activate interpolation
