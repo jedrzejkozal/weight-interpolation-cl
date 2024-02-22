213fae03b0c7130fa39b887ced0c441e70eb8fc9
jedrzejkozal <jedrzej.kozal@gmail.com>
2024-02-22 11:20:24 +0100

    update run_template.sh

--- run_template.sh
+++ run_template.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 
+if [[ $(conda --version) == "conda: command not found" ]]; then
+    /opt/anaconda/condabin/conda init
+    source ~/.bashrc
+
+    source activate base
+    conda install -n base conda-libmamba-solver -y
+    conda config --set solver libmamba
+fi
 
 if { conda env list | grep 'interpolation'; } >/dev/null 2>&1; then
     echo "env exists"
 else
-    conda env create -f env.yml
+    conda env create -f env.yml -y
 fi
+source activate interpolation
 
 # enter your code here
 
