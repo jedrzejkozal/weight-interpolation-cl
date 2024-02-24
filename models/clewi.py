94669105affd8a293fa18f959465e7afa56f68c6
jedrzejkozal <jedrzej.kozal@gmail.com>
2024-02-24 10:36:19 +0100

    add gradient penalty

--- models/clewi.py
+++ models/clewi.py
@@ -18,6 +18,7 @@ def get_parser() -> ArgumentParser:
     parser.add_argument('--permuation_epochs', type=int, default=1)
     parser.add_argument('--batchnorm_epochs', type=int, default=1)
     parser.add_argument('--debug_interpolation', action='store_true')
+    parser.add_argument('--lambda_grad_penalty', type=float, default=0.1)
 
     add_management_args(parser)
     add_experiment_args(parser)
@@ -48,8 +49,14 @@ class Clewi(ContinualModel):
             inputs = torch.cat((inputs, buf_inputs))
             labels = torch.cat((labels, buf_labels))
 
+        inputs.requires_grad = True
         outputs = self.net(inputs)
         loss = self.loss(outputs, labels)
+        penalty = self.args.lambda_grad_penalty * self.calc_gradient_penalty(inputs, outputs)
+        if not self.first_task and not torch.isnan(penalty):
+            loss += penalty
+        if torch.isnan(penalty):
+            print('isnan')
 
         # ht = hessian_trace(self.net, loss, self.device, 10)
         # print(ht)
@@ -61,6 +68,22 @@ class Clewi(ContinualModel):
                              labels=labels[:real_batch_size])
 
         return loss.item()
+
+    @staticmethod
+    def calc_gradient_penalty(x, y_pred):
+        gradients = torch.autograd.grad(
+            outputs=y_pred,
+            inputs=x,
+            grad_outputs=torch.ones_like(y_pred),
+            create_graph=True,
+        )[0]
+
+        gradients = gradients.flatten(start_dim=1)
+        grad_norm = gradients.norm(2, dim=1)
+        gradient_penalty = ((grad_norm - 1) ** 2).mean()
+    #     gradient_penalty = F.relu(grad_norm - 1).mean()
+
+        return gradient_penalty
 
     def end_task(self, dataset):
         print('\n\n')
@@ -109,15 +132,44 @@ class Clewi(ContinualModel):
         torch.save(self.old_model, 'old_model.pt')
         torch.save(self.net, 'net.pt')
 
+        L2_dist, diff_average, diff_sum, max_diff, min_diff, n = self.weight_difference(self.net, self.old_model)
+        print(f'weight_difference: L2 = {L2_dist}, diff_average = {diff_average}, diff_sum = {diff_sum}, max_diff = {max_diff}, min_diff = {min_diff}, n = {n}')
+        print('losses before interpolation:')
+        _, losses, ns = evaluate(self.old_model, dataset, self.device, reduction='sum')
+        print(f'old model: losses = {losses}, ns = {ns}')
+        _, losses, ns = evaluate(self.net, dataset, self.device, reduction='sum')
+        print(f'new model: losses = {losses}, ns = {ns}')
+
         buffer_dataloder = self.get_buffer_dataloder()
         if self.args.debug_interpolation:
             self.interpolation_plot(dataset, buffer_dataloder)
         self.old_model = interpolate(self.net, self.old_model, buffer_dataloder, self.device, alpha=self.interpolation_alpha,
                                      permuation_epochs=self.args.permuation_epochs, batchnorm_epochs=self.args.batchnorm_epochs)
         self.net = self.deepcopy_model(self.old_model)
+        print('losses after interpolation:')
+        _, losses, ns = evaluate(self.old_model, dataset, self.device, reduction='sum')
+        print(f'interpolated model: losses = {losses}, ns = {ns}')
         # self.train_model_after_interpolation(buffer_dataloder)
         self.opt = self.opt.__class__(self.net.parameters(), **self.opt.defaults)
         self.opt.zero_grad()
+
+    def weight_difference(self, net1, net2):
+        diff_sum = 0.0
+        diff_sq_sum = 0.0
+        max_diff = 0.0
+        min_diff = 100000000.0
+        n = 0
+        with torch.no_grad():
+            for p1, p2 in zip(net1.state_dict().values(), net2.state_dict().values()):
+                diff = p1 - p2
+                diff_sum += (p1 - p2).sum().item()
+                diff_sq_sum += torch.square(p1 - p2).sum().item()
+                max_diff = max(max_diff, torch.max(diff).item())
+                min_diff = min(min_diff, torch.min(diff).item())
+                n += torch.numel(p1)
+        diff_average = diff_sum / n
+        L2_dist = torch.sqrt(torch.Tensor([diff_sq_sum])).item()
+        return L2_dist, diff_average, diff_sum, max_diff, min_diff, n
 
     def interpolation_plot(self, dataset, buffer_dataloader):
         alpha_grid = np.arange(0, 1.001, 0.02)
@@ -126,15 +178,15 @@ class Clewi(ContinualModel):
             net = self.deepcopy_model(self.net)
             old = self.deepcopy_model(self.old_model)
             new_model = interpolate(net, old, buffer_dataloader, self.device, alpha=alpha)
-            acc = evaluate(new_model, dataset, self.device)
+            acc, _, _ = evaluate(new_model, dataset, self.device)
             interpolation_accs.append(acc)
         print('interpolation accuracies:')
         print(interpolation_accs)
         print('old model accs:')
-        accs = evaluate(self.old_model, dataset, self.device)
+        accs, _, _ = evaluate(self.old_model, dataset, self.device)
         print(accs)
         print('new model accs:')
-        accs = evaluate(self.net, dataset, self.device)
+        accs, _, _ = evaluate(self.net, dataset, self.device)
         print(accs)
 
     def get_buffer_dataloder(self, batch_size=32):
@@ -181,15 +233,18 @@ class Clewi(ContinualModel):
 
 
 @torch.no_grad()
-def evaluate(network: ContinualModel, dataset, device, last=False):
+def evaluate(network: ContinualModel, dataset, device, last=False, reduction='mean'):
     status = network.training
     network.eval()
     network.to(device)
+    losses = []
     accs = []
+    ns = []
     for k, test_loader in enumerate(dataset.test_loaders):
         if last and k < len(dataset.test_loaders) - 1:
             continue
         correct, total = 0.0, 0.0
+        loss = 0.0
         for data in test_loader:
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
@@ -197,9 +252,12 @@ def evaluate(network: ContinualModel, dataset, device, last=False):
 
             _, pred = torch.max(outputs.data, 1)
             correct += torch.sum(pred == labels).item()
+            loss += F.cross_entropy(outputs, labels, reduction=reduction)
             total += labels.shape[0]
 
         accs.append(correct / total * 100)
+        losses.append(loss)
+        ns.append(total)
 
     network.train(status)
-    return accs
+    return accs, losses, ns
